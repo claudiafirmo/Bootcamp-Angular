3b6bf4d40bb0af72d43b9794848fb0fc7b500007
mandaapag03 <paganiamanda791@gmail.com>
2022-10-28 18:05:16 -0300

    Adiciona aula 28/10

--- FrontEnd/aula2_aula3/01_bootstrap/js/eventoDias.js
+++ FrontEnd/aula2_aula3/01_bootstrap/js/eventoDias.js
@@ -2,9 +2,16 @@
 
 //executando evento click no botao
 document.getElementById('btnCalcular').addEventListener('click', function () {
-    let d = parseInt(document.getElementById("dia").value);
-    let m = parseInt(document.getElementById("mes").value);
-    let a = parseInt(document.getElementById("ano").value);
 
-    document.getElementById("resposta").innerHTML = "<p> Restam <strong>" + calcularDias(dia=d, mes=m, ano=a) + "</strong> dias para acabar o ano</p>"
+    try {
+        let d = parseInt(document.getElementById("dia").value);
+        let m = parseInt(document.getElementById("mes").value);
+        let a = parseInt(document.getElementById("ano").value);
+    
+        document.getElementById("resposta").innerHTML = "<p> Restam <strong>" + calcularDias(dia=d, mes=m, ano=a) + "</strong> dias para acabar o ano</p>";
+    }catch(error){ // error -> é uma variável que contem o tipo de erro, poderia ser outro nome
+        //alert('Erro: ' + error);
+        document.getElementById('resposta').innerHTML = error.message;
+    }
+
 });
