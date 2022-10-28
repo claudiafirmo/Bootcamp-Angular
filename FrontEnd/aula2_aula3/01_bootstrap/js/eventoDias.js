fc509f2dac7c4cf01cbf1d03a07e99fa7e1c9f4b
mandaapag03 <paganiamanda791@gmail.com>
2022-10-28 16:01:40 -0300

    Adiciona parte 1 da aula 28/10 e apostilas front

---
+++ FrontEnd/aula2_aula3/01_bootstrap/js/eventoDias.js
@@ -0,0 +1,10 @@
+// arquivo dependente do HTML
+
+//executando evento click no botao
+document.getElementById('btnCalcular').addEventListener('click', function () {
+    let d = parseInt(document.getElementById("dia").value);
+    let m = parseInt(document.getElementById("mes").value);
+    let a = parseInt(document.getElementById("ano").value);
+
+    document.getElementById("resposta").innerHTML = "<p> Restam <strong>" + calcularDias(dia=d, mes=m, ano=a) + "</strong> dias para acabar o ano</p>"
+});
