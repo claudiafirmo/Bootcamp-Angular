ea874b31a55a2831c30c737a6fde4984a9af730b
mandaapag03 <paganiamanda791@gmail.com>
2022-11-22 17:41:02 -0300

    Adiciona Projeto 3

---
+++ CSharp/Projeto02_Variaveis/Projeto02_Variaveis/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02_Variaveis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metodos.ApresentarVariaveis();
+            //MostrarCurso();
+            //Metodos.LerVariaveis();
+            
+            Console.Write("Informe o peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe altura: ");
+            double altura = double.Parse(Console.ReadLine());
+
+            //double imc = Metodos.CalcularImc(peso, altura);
+            Console.WriteLine($"Altura: {altura}, peso {peso}. Seu IMC é {Metodos.CalcularImc(peso, altura):0.00}");
+
+            Console.WriteLine($"Número gerado: {Metodos.CalcularValorAleatorio()}");
+            Console.WriteLine($"Número gerado: {Metodos.CalcularValorAleatorio()}");
+            Console.WriteLine($"Número gerado: {Metodos.CalcularValorAleatorio()}");
+            Console.WriteLine($"Número gerado: {Metodos.CalcularValorAleatorio()}");
+
+            Console.ReadKey();
+        }
+        static void MostrarCurso()
+        {
+            Console.WriteLine("Cursos da semana:");
+            Console.WriteLine("Linguagem C#");
+            Console.WriteLine("Python");
+        }
+    }
+}
---
+++ CSharp/Projeto03_Estruturas/Projeto03_Estruturas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto03_Estruturas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Variaveis do C# X Variaveis do Framework
+             * 
+             * Os tipos do C# possuem tipos análogos no Framework
+             * No processo de compilação, os tipos considerados primitivos
+             * são convertidos nos tipos do framework.
+             */
+            int x1 = 100;
+            Int32 x2 = 100;
+
+            double d1 = 1.5;
+            Double d2 = 1.5;
+
+            string s1 = "texto";
+            String s2 = "texto";
+
+            Console.WriteLine(s1);
+            Console.ReadKey();
+        }
+    }
+}
