a620e3c14e4401de61dc7249a6526f6670eefe70
mandaapag03 <paganiamanda791@gmail.com>
2022-11-29 18:02:39 -0300

    Adiciona aula 29/11 - POO

---
+++ CSharp/Projeto05_POO/Projeto01_ClassesObjetos/Program.cs
@@ -0,0 +1,29 @@
+﻿using ClassesObjetos.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instanciando a classe pessoa e fazendo com que a variável p1 referencie essa instancia
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Amanda";
+            p1.Idade = 19;
+
+            // Atribuindo referencias: A variavel p2 referencia o mesmo objeto referenciado pela variavel p1
+            Pessoa p2 = p1;
+            p2.Idade = 20;
+
+            Console.WriteLine(p2.Equals(p1));
+
+            Console.WriteLine($"Nome informado: {p1.Nome}\nIdade informada: {p1.Idade}");
+            Console.ReadKey();
+        }
+    }
+}
