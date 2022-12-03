b33de0dea16f8a9d7bc671bbecb6b18206bbf3b4
mandaapag03 <paganiamanda791@gmail.com>
2022-12-03 19:21:06 -0300

    Adiciona estudos da apostila C#

---
+++ CSharp/EstudandoApostilaCSharp/ClassesAbstratasVSInterfaces/Interface/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAbstratasVSInterfaces.Interface
+{
+    public interface Animal
+    {
+        string Nome { get; set; }
+        int Idade { get; set; }
+
+        string EmitirSom();
+    }
+}
---
+++ CSharp/EstudandoApostilaCSharp/ClassesAbstratasVSInterfaces/Interface/Gato.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAbstratasVSInterfaces.Interface
+{
+    public class Gato: Animal
+    {
+        public string Nome { get; set; }
+
+        private int _idade;
+        public int Idade
+        {
+            get => _idade;
+            set
+            {
+                if (_idade < 0)
+                {
+                    throw new InvalidOperationException("Idade inválida! Deve ser um valor maior ou igual a 0");
+                }
+                _idade = value;
+            }
+        }
+        public string EmitirSom()
+        {
+            return "MIAU";
+        }
+    }
+}
