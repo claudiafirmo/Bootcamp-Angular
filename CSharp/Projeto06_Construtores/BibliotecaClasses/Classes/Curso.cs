8df6cd42b45bd65fadfd80b701049480dc9c41ac
mandaapag03 <paganiamanda791@gmail.com>
2022-12-07 17:52:25 -0300

    Adiciona Aula 07/12 Construtores e Interface

---
+++ CSharp/Projeto06_Construtores/BibliotecaClasses/Classes/Curso.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos
+{
+    public class Curso
+    {
+
+        public Curso(string Descricao, int CargaHoraria)
+        {
+            this.Descricao = Descricao;
+            this.CargaHoraria = CargaHoraria;
+        }
+
+        private string _descricao;
+        public string Descricao
+        { 
+            get => _descricao;
+            set => _descricao = !string.IsNullOrEmpty(value) ? value :
+                throw new Exception("Descrição Inválida");
+        }
+        
+        private int _ch;
+        public int CargaHoraria 
+        {
+            get => _ch; 
+            set => _ch = value > 0 ? value : throw new Exception("A carga horária deve ser positiva"); 
+        }
+
+        public string LerCurso()
+        {
+            return $"Descrição : {Descricao}\nCarga Horária: {CargaHoraria}";
+        }
+    }
+}
