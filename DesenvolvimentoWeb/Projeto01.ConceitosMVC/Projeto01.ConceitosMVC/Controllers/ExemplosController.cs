1dc82140d600fcfc0f664abb25727cc80de94a0d
mandaapag03 <paganiamanda791@gmail.com>
2022-12-14 17:58:31 -0300

    Adiciona aula 14/12 - MVC e ASP.NET Core

---
+++ DesenvolvimentoWeb/Projeto01.ConceitosMVC/Projeto01.ConceitosMVC/Controllers/ExemplosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto01.ConceitosMVC.Controllers
+{
+    public class ExemplosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public string ExibirTexto()
+        {
+            return "<h2>Retorno de uma string gerada pelo action</h2>";
+        }
+        public ViewResult Conteudo()
+        {
+            return View();
+        }
+    }
+}
---
+++ DesenvolvimentoWeb/Projeto01.ConceitosMVC/Projeto01.ConceitosMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projeto01.ConceitosMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
