dd8c8998c527562f87fc83acebfa78a257ee5454
mandaapag03 <paganiamanda791@gmail.com>
2022-12-26 17:55:46 -0300

    Adiciona Aula 26/12 - Exercício Listar eventos por convidado

--- DesenvolvimentoWeb/Projeto02.GestaoEventos/Projeto02.GestaoEventos/Controllers/InscricoesController.cs
+++ DesenvolvimentoWeb/Projeto02.GestaoEventos/Projeto02.GestaoEventos/Controllers/InscricoesController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
+using Microsoft.Win32;
 using Projeto02.GestaoEventos.DataAccess;
 using Projeto02.GestaoEventos.Models;
+using Projeto02.GestaoEventos.ViewModel;
 
 namespace Projeto02.GestaoEventos.Controllers
 {
@@ -43,7 +46,7 @@ namespace Projeto02.GestaoEventos.Controllers
             try
             {
                 inscricoesDao.Incluir(inscricao);
-                return RedirectToAction("Index");
+                return RedirectToAction("ListarConvidados");
             }
             catch (Exception ex)
             {
@@ -52,12 +55,52 @@ namespace Projeto02.GestaoEventos.Controllers
             }
         }
         [HttpGet]
-        public IActionResult ListarConvidados(int id)
+        public IActionResult ListarConvidados(int codigoEvento)
         {
             try
             {
                 ViewBag.ListaEventos = new SelectList(eventosDao.ListarTodos(), "id", "descricao");
-                return View(inscricoesDao.ListarConvidados(id));
+                return View(inscricoesDao.ListarConvidados(codigoEvento));
+            }
+            catch (Exception ex)
+            {
+
+                return View("_Erro", ex);
+            }
+        }
+        [HttpGet]
+        public IActionResult ListarEventos(int codigoConvidado)
+        {
+            try
+            {
+                ViewBag.ListaConvidados = new SelectList(convidadosDao.ListarTodos(), "id", "nome");
+                return View(inscricoesDao.ListarEventos(codigoConvidado));
+
+            }
+            catch(Exception ex)
+            {
+                return View("_Erro", ex);
+            }
+        }
+        [HttpGet]
+        public IActionResult ExcluirInscricao(int id)
+        {
+            try
+            {
+                return View(inscricoesDao.Buscar(id));
+            }
+            catch (Exception ex) 
+            {
+                return View("_Erro", ex);
+            }
+        }
+        [HttpPost]
+        public IActionResult ExcluirInscricao(EventosConvidadosVM registro)
+        {
+            try
+            {
+                inscricoesDao.Excluir(registro);
+                return RedirectToAction("ListarEventos");
             }
             catch (Exception ex)
             {
--- DesenvolvimentoWeb/Projeto02.GestaoEventos/Projeto02.GestaoEventos/DataAccess/InscricoesDao.cs
+++ DesenvolvimentoWeb/Projeto02.GestaoEventos/Projeto02.GestaoEventos/DataAccess/InscricoesDao.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using Dapper.Contrib.Extensions;
 using Projeto02.GestaoEventos.Models;
 using Projeto02.GestaoEventos.ViewModel;
 
@@ -42,6 +43,56 @@ namespace Projeto02.GestaoEventos.DataAccess
                 }
                 sql += " WHERE e.id = @idEvento";
                 return Conn.Query<EventosConvidadosVM>(sql, new{ idEvento });
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+        public IEnumerable<EventosConvidadosVM> ListarEventos(int idConvidado)
+        {
+            try
+            {
+                AbrirConexao();
+                string sql = "SELECT i.id as \"Id\", e.descricao as \"Evento\", e.data as \"DataEvento\", c.nome as \"Convidado\", i.datainscricao as \"DataInscricao\"" +
+                    " FROM administracao.tb_inscricoes i INNER JOIN administracao.tb_eventos e on e.id = i.idevento" +
+                    " INNER JOIN administracao.tb_convidados c on i.idconvidado = c.id";
+
+                if(idConvidado == 0)
+                {
+                    return Conn.Query<EventosConvidadosVM>(sql);
+                }
+                sql += " WHERE c.id = @idConvidado;";
+                return Conn.Query<EventosConvidadosVM>(sql, new { idConvidado });
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+        public EventosConvidadosVM Buscar(int idInscricao)
+        {
+            try
+            {
+                AbrirConexao();
+                string sql = "SELECT i.id as \"Id\", e.descricao as \"Evento\", e.data as \"DataEvento\", c.nome as \"Convidado\", i.datainscricao as \"DataInscricao\"" +
+                    " FROM administracao.tb_inscricoes i INNER JOIN administracao.tb_eventos e on e.id = i.idevento" +
+                    " INNER JOIN administracao.tb_convidados c on i.idconvidado = c.id" +
+                    " WHERE i.id = @idInscricao";
+                return Conn.QueryFirst<EventosConvidadosVM>(sql, new { idInscricao });
+            }
+            finally
+            {
+                FecharConexao();
+            }
+        }
+        public int Excluir(EventosConvidadosVM inscricao)
+        {
+            try
+            {
+                AbrirConexao();
+                string sql = "DELETE FROM administracao.tb_inscricoes WHERE id = @id";
+                return Conn.Execute(sql, new { id = inscricao.Id });
             }
             finally
             {
