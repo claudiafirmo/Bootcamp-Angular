0fbc70b00a3fb77d507a2b12beffaea14e688e6c
mandaapag03 <paganiamanda791@gmail.com>
2023-01-03 19:03:10 -0300

    Adiciona aula 03/01 - Exercicio consumo API com JS, consumo API no MVC, Injeção de Dependência

---
+++ DesenvolvimentoWeb/Projeto02.GestaoEventos/Projeto02.GestaoEventos/Controllers/ApiConvidadosController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto02.GestaoEventos.Models;
+using System.Net.Http.Headers;
+
+namespace Projeto02.GestaoEventos.Controllers
+{
+    // Controller que administra consumo de uma API
+    public class ApiConvidadosController : Controller
+    {
+        HttpClient client;
+
+        // Injeção de dependencia
+        public ApiConvidadosController(IHttpClientFactory httpClient)
+        {
+            client = httpClient.CreateClient();
+        }
+
+        // Criar action que lista os convidados API
+        public async Task<IActionResult> Listar()
+        {
+            client.BaseAddress = new Uri("http://localhost:5192/");
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            try
+            {
+                HttpResponseMessage response = client.GetAsync("api/guest/").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var convidados = await response.Content.ReadAsAsync<Convidado[]>();
+                    return View(convidados.ToList());
+                }
+                else
+                {
+                    throw new Exception("Ocorreu um erro na listagem");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return View("_Erro");
+            }
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DesenvolvimentoWeb/Projeto04.ConsumoWebApiJs/app.js
+++ DesenvolvimentoWeb/Projeto04.ConsumoWebApiJs/app.js
@@ -1,4 +1,41 @@
+function listar() {
+    $.ajax({
+        dataType: "json",
+        url: "http://localhost:5192/api/guest/",
+        method: "GET",
+        success: function (resposta) {
+            $("#tabela > tbody:has(tr)").empty();
+            $.each(resposta, function (index, item) {
+                $("#tabela > tbody").append(`<tr>
+                    <td>${item.id}</td>
+                    <td>${item.cpf}</td>
+                    <td>${item.nome}</td>
+                    <td>${item.email}</td>
+                    <td><button type="button" class="btn btn-danger" onclick="remover(${item.id})">Remover</button>
+                </tr>`)
+            });
+        },
+        error: function (erro) {
+            alert(erro.responseText);
+        }
+
+    });
+}
+
+function remover(id) {
+    if (window.confirm("Tem certeza que deseja remover esse convidado?")) {
+        $.ajax({
+            dataType: "json",
+            url: "http://localhost:5192/api/guest/" + id,
+            method: "DELETE"
+        }).done(function () {
+            listar();
+        });
+    }
+}
+
 $(document).ready(function () {
+    listar();
     // instrução para adicionar um novo convidado
     $("#btnIncluir").click(function () {
 
@@ -26,6 +63,7 @@ $(document).ready(function () {
             success: function (resposta) {
                 $("#resposta").addClass("alert alert-success");
                 $("#resposta").html("Convidado incluido com sucesso!");
+                listar();
             },
             error: function (erro) {
                 $("#resposta").addClass("alert alert-error");
