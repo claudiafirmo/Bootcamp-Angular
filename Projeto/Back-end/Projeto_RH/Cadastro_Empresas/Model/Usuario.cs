e08704e56d676ea7572d67ecf77c4344f46d85b2
mandaapag03 <paganiamanda791@gmail.com>
2023-01-28 19:34:37 -0300

    Separa projeto do repositorio do bootcamp

--- Projeto/Back-end/Projeto_RH/Cadastro_Empresas/Model/Usuario.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cadastro_Empresas.Model
-{
-    public class Usuario
-    {
-        public string? Nome{ get; set; }
-        public string? Senha { get; set; }
-        public string? Nivel { get; set; }
-    }
-}
